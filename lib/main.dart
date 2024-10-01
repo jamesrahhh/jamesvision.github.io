2bedd75e133d6c8fd1cae1b19e983adc809035d5
jamesrahhh <juntaotto777@gmail.com>
2024-09-30 22:20:26 -0500

    meta: Recode.

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:jamesrahhhgithubio/pages/home.dart';
 
 void main() {
-  runApp(const JamesrahhhGithubIo());
+  runApp(const Jamesrahhh());
 }
 
-class JamesrahhhGithubIo extends StatelessWidget {
-  const JamesrahhhGithubIo({super.key});
+class Jamesrahhh extends StatelessWidget {
+  const Jamesrahhh({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/home',
-      routes: {
-        '/home': (context) => const Home(),
-      },
-      debugShowCheckedModeBanner: false,
+      title: 'jamesrahhh.dev',
+      theme: ThemeData(useMaterial3: true),
+      home: const Home(),
     );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Text>[
+          Text('@jamesrahhh',
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                  fontSize: 15,
+                  decoration: TextDecoration.none,
+                  color: Colors.black,
+                  fontFamily: 'Montserrat',
+                  fontVariations: <FontVariation>[FontVariation('wght', 700)])),
+          Text(
+            'coming soon.',
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+                fontSize: 100,
+                decoration: TextDecoration.none,
+                color: Colors.black,
+                fontFamily: 'Montserrat',
+                fontVariations: <FontVariation>[FontVariation('wght', 700)]),
+          ),
+        ]);
   }
 }
