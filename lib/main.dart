a1dc622582aeead7b5b2ace4642c6cd86b5d09ba
jamesrahhh <juntaotto777@gmail.com>
2024-10-07 22:51:15 -0500

    main: Implement basic theme and license page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -13,8 +14,50 @@ class Jamesrahhh extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'jamesrahhh.dev',
-      theme: ThemeData(
-          textTheme: Typography.blackMountainView, useMaterial3: true),
+      theme: FlexThemeData.light(
+        textTheme: const TextTheme(
+          displaySmall: TextStyle(
+              fontSize: 15,
+              fontVariations: <FontVariation>[FontVariation('wght', 800)]),
+          displayMedium: TextStyle(
+              fontVariations: <FontVariation>[FontVariation('wght', 800)]),
+          displayLarge: TextStyle(
+              fontSize: 100,
+              fontVariations: <FontVariation>[FontVariation('wght', 800)]),
+          headlineSmall: TextStyle(
+              fontVariations: <FontVariation>[FontVariation('wght', 700)]),
+          headlineMedium: TextStyle(
+              fontVariations: <FontVariation>[FontVariation('wght', 700)]),
+          headlineLarge: TextStyle(
+              fontVariations: <FontVariation>[FontVariation('wght', 700)]),
+          titleSmall: TextStyle(
+              fontVariations: <FontVariation>[FontVariation('wght', 600)]),
+          titleMedium: TextStyle(
+              fontVariations: <FontVariation>[FontVariation('wght', 600)]),
+          titleLarge: TextStyle(
+              fontVariations: <FontVariation>[FontVariation('wght', 600)]),
+          bodySmall: TextStyle(
+              fontVariations: <FontVariation>[FontVariation('wght', 400)]),
+          bodyMedium: TextStyle(
+              fontVariations: <FontVariation>[FontVariation('wght', 400)]),
+          bodyLarge: TextStyle(
+              fontVariations: <FontVariation>[FontVariation('wght', 400)]),
+        ).apply(
+          bodyColor: Colors.black,
+          displayColor: Colors.black,
+        ),
+        colors: const FlexSchemeColor(
+          primary: Colors.black,
+          primaryContainer: Colors.black,
+          secondary: Colors.black,
+          secondaryContainer: Colors.black,
+          tertiary: Colors.black,
+          tertiaryContainer: Colors.white,
+          error: Colors.red,
+          errorContainer: Colors.white,
+        ),
+        fontFamily: 'Montserrat',
+      ),
       home: const Home(),
     );
   }
@@ -25,63 +68,55 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: <Widget>[
-      Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-        const Text('@jamesrahhh',
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(
-                fontSize: 15,
-                decoration: TextDecoration.none,
-                color: Colors.black,
-                fontFamily: 'Montserrat',
-                fontVariations: <FontVariation>[FontVariation('wght', 700)])),
-        const FittedBox(
-          fit: BoxFit.fitWidth,
-          child: Text(
-            'coming soon.',
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(
-                fontSize: 100,
-                decoration: TextDecoration.none,
-                color: Colors.black,
-                fontFamily: 'Montserrat',
-                fontVariations: <FontVariation>[FontVariation('wght', 700)]),
-          ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <GestureDetector>[
-            GestureDetector(
-                onTap: () => launchUrl(Uri.https('github.com', 'jamesrahhh')),
-                child: const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Icon(
-                      FontAwesomeIcons.github,
-                      color: Colors.black,
-                      size: 25,
-                    ))),
-            GestureDetector(
-                onTap: () =>
-                    launchUrl(Uri.https('instagram.com', 'jamesrahhh')),
-                child: const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Icon(
-                      FontAwesomeIcons.instagram,
-                      color: Colors.black,
-                      size: 25,
-                    )))
-          ],
-        )
-      ]),
-      Align(
-          alignment: Alignment.bottomRight,
-          child: GestureDetector(
-            onTap: () => showLicensePage(context: context),
-            child: const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Icon(Icons.info_outline, color: Colors.black),
+    return Scaffold(
+      body: Stack(children: <Widget>[
+        Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+          Text('@jamesrahhh',
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context).textTheme.displaySmall),
+          FittedBox(
+            fit: BoxFit.fitWidth,
+            child: Text(
+              'coming soon.',
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context).textTheme.displayLarge,
             ),
-          ))
-    ]);
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <GestureDetector>[
+              GestureDetector(
+                  onTap: () => launchUrl(Uri.https('github.com', 'jamesrahhh')),
+                  child: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Icon(
+                        FontAwesomeIcons.github,
+                        color: Colors.black,
+                        size: 25,
+                      ))),
+              GestureDetector(
+                  onTap: () =>
+                      launchUrl(Uri.https('instagram.com', 'jamesrahhh')),
+                  child: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Icon(
+                        FontAwesomeIcons.instagram,
+                        color: Colors.black,
+                        size: 25,
+                      )))
+            ],
+          )
+        ]),
+        Align(
+            alignment: Alignment.bottomRight,
+            child: GestureDetector(
+              onTap: () => showLicensePage(context: context),
+              child: const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Icon(Icons.info_outline, color: Colors.black),
+              ),
+            ))
+      ]),
+    );
   }
 }
